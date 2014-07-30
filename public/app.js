e06ff1d1165f1cc59a46f902a8c4ba8442150353
Peter Gasston <peter@rehabstudio.com>
2014-07-30 16:35:21 -0400

    Added ‘gulps’

--- public/app.js
+++ public/app.js
@@ -4,15 +4,16 @@ var quantity = 100,
     fps = 24;
 
 btn.addEventListener('click', function () {
+  // Disable button and send message that button has been pushed
   btn.setAttribute('disabled',true);
-  console.log('pushed button');
-  socket.emit('btn-push', 'OK');
+  socket.emit('btn-push');
 });
 
 function drinking () {
   // This is the function that controls draining the bottle; at the moment it just runs down automatically from 100%, need to change this to respond to input from socket.
   console.log('drinking');
-  var liquid = document.getElementById('liquid');
+  var liquid = document.getElementById('liquid'),
+      gulp;
   (function drain () {
     window.setTimeout(function () {
       if (quantity > 0) {
@@ -20,13 +21,17 @@ function drinking () {
       } else {
         socket.emit('stopped');
       }
-      liquid.style.height = quantity + '%';
+      gulp = quantity % 15 === 0;
+      if (gulp) {
+        liquid.style.height = quantity + '%';
+      }
       quantity--;
-    }, 100 / fps);
+    }, 1000 / fps);
   })()
 }
 
 socket.on('btn-pushed', function () {
+  // Reset liquid quantity and run the draining when the button has been pushed
   console.log('ready to drink');
   // document.body.classList.toggle('on');
   if (quantity < 100) {
@@ -36,5 +41,6 @@ socket.on('btn-pushed', function () {
 });
 
 socket.on('btn-active', function () {
+  // Make the button active again when the draining finishes
   btn.removeAttribute('disabled');
 });
